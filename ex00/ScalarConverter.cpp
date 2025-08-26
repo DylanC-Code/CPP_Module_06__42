9aace0f7c369a370e2d316195d2606b3efb1da7a
dcastor <dcastor@student.42.fr>
2025-08-26 12:52:37 +0200

    feat(ex00): ScalarConverter relou!

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,163 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 09:38:20 by dcastor           #+#    #+#             */
+/*   Updated: 2025/08/26 12:50:04 by dcastor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+void ScalarConverter::converter(const std::string &scalar)
+{
+	ScalarConverter::handleChar(scalar);
+	if (isPseudoLiterals(scalar))
+		ScalarConverter::handlePseudoLiterals(scalar);
+	else if (isInt(scalar))
+		ScalarConverter::handleInt(scalar);
+	else if (isFloat(scalar))
+		ScalarConverter::handleFloat(scalar);
+	else if (isDouble(scalar))
+		ScalarConverter::handleDouble(scalar);
+	else
+	{
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
+	}
+}
+
+/* Handlers */
+
+void ScalarConverter::handleChar(const std::string &scalar)
+{
+	if (scalar.length() == 1)
+	{
+		char c = static_cast<char>(scalar[0]);
+		std::cout << "char: " << c << std::endl;
+		return;
+	}
+	std::cout << "char: Non displayable" << std::endl;
+}
+
+void ScalarConverter::handleInt(const std::string &scalar)
+{
+	int val = atoi(scalar.c_str());
+
+	std::cout << "int: " << val << std::endl;
+	std::cout << "float: " << static_cast<float>(val) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(val) << ".0" << std::endl;
+}
+
+void ScalarConverter::handlePseudoLiterals(const std::string &scalar)
+{
+	if (scalar == "nan" || scalar == "nanf")
+	{
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+	}
+	else if (scalar == "-inff" || scalar == "-inf")
+	{
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: -inff" << std::endl;
+		std::cout << "double: -inf" << std::endl;
+	}
+	else if (scalar == "+inff" || scalar == "+inf")
+	{
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: +inff" << std::endl;
+		std::cout << "double: +inf" << std::endl;
+	}
+}
+
+void ScalarConverter::handleFloat(const std::string &scalar)
+{
+	size_t len = scalar.length();
+	std::string number = scalar.substr(0, len - 1);
+	float val = atof(number.c_str());
+
+	if (val < static_cast<float>(INT_MIN) || val > static_cast<float>(INT_MAX))
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << static_cast<int>(val) << std::endl;
+	std::cout << "float: " << val << "f" << std::endl;
+	std::cout << "double: " << static_cast<double>(val) << std::endl;
+}
+
+void ScalarConverter::handleDouble(const std::string &scalar)
+{
+	double val = atof(scalar.c_str());
+
+	if (val < INT_MIN || val > INT_MAX)
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << static_cast<int>(val) << std::endl;
+	if (val < -FLT_MAX || val > FLT_MAX)
+		std::cout << "float: impossible" << std::endl;
+	else
+		std::cout << "float: " << static_cast<float>(val) << "f" << std::endl;
+	std::cout << "double: " << val << std::endl;
+}
+
+/* Checkers */
+
+bool ScalarConverter::isInt(const std::string &scalar)
+{
+	int out = 0;
+	std::istringstream iss(scalar);
+	iss >> out;
+	return !(iss.fail() || !iss.eof());
+}
+
+bool ScalarConverter::isPseudoLiterals(const std::string &scalar)
+{
+	const std::string literals[6] = {"nan", "nanf", "-inff", "+inff", "-inf", "+inf"};
+
+	for (size_t i = 0; i < 6; i++)
+		if (literals[i] == scalar)
+			return true;
+	return false;
+}
+
+bool ScalarConverter::isFloat(const std::string &scalar)
+{
+	size_t len = scalar.length();
+
+	if (len < 2 || scalar[len - 1] != 'f')
+		return false;
+
+	std::string number = scalar.substr(0, len - 1);
+	errno = 0;
+	char *end;
+	double val = std::strtod(number.c_str(), &end);
+
+	if (*end != '\0')
+		return false;
+	if ((val == HUGE_VAL || val == -HUGE_VAL) && errno == ERANGE)
+		return false;
+	if (val == 0.0 && errno == ERANGE)
+		return false;
+	if (val > static_cast<double>(FLT_MAX) || val < -static_cast<double>(FLT_MAX))
+		return false;
+	return true;
+}
+
+bool ScalarConverter::isDouble(const std::string &scalar)
+{
+	errno = 0;
+	char *end;
+	double val = std::strtod(scalar.c_str(), &end);
+
+	if (*end != '\0')
+		return false;
+	if ((val == HUGE_VAL || val == -HUGE_VAL) && errno == ERANGE)
+		return false;
+	if (val == 0.0 && errno == ERANGE)
+		return false;
+	return true;
+}
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 09:35:17 by dcastor           #+#    #+#             */
-/*   Updated: 2025/08/26 09:37:09 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/08/26 12:49:27 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,28 @@
 #define SCALAR_CONVERTER_HPP
 
 #include <string>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <cmath>
+#include <cfloat>
+#include <climits>
+#include <errno.h>
 
 class ScalarConverter
 {
+private:
+	static void handleChar(const std::string &scalar);
+	static void handleInt(const std::string &scalar);
+	static void handlePseudoLiterals(const std::string &scalar);
+	static void handleFloat(const std::string &scalar);
+	static void handleDouble(const std::string &scalar);
+
+	static bool isInt(const std::string &scalar);
+	static bool isPseudoLiterals(const std::string &scalar);
+	static bool isFloat(const std::string &scalar);
+	static bool isDouble(const std::string &scalar);
+
 public:
 	static void converter(const std::string &scalar);
 };
