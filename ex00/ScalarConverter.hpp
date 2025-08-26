57155e6af6295854a08967af07afff4bfdf3f85b
dcastor <dcastor@student.42.fr>
2025-08-26 09:38:05 +0200

    feat(ex00): ScalarConverter declarations done

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 09:35:17 by dcastor           #+#    #+#             */
+/*   Updated: 2025/08/26 09:37:09 by dcastor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_HPP
+#define SCALAR_CONVERTER_HPP
+
+#include <string>
+
+class ScalarConverter
+{
+public:
+	static void converter(const std::string &scalar);
+};
+
+#endif
