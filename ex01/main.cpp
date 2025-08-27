ca808d4d503c26d9ba7a7726716f30bf3d140e11
dcastor <dcastor@student.42.fr>
2025-08-27 10:16:46 +0200

    feat(ex01): Makefile and main added

---
+++ ex01/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 10:15:08 by dcastor           #+#    #+#             */
+/*   Updated: 2025/08/26 10:15:49 by dcastor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 2)
+		return 1;
+	ScalarConverter::converter(argv[1]);
+	return 0;
+}
